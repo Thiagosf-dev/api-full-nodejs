acbb0c70a876451e8f13a2b3df425bd8446a008e
Thiagosf-dev <thiagosf.dev@gmail.com>
2019-03-24 21:34:04 -0400

    Instalando e configurando o mongoose

--- src/app.js
+++ src/app.js
@@ -4,9 +4,18 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const requireDir = require('require-dir');
+const mongoose = require('mongoose');
+
+// CONEXÃO BANCO
+mongoose.connect('mongodb://root:root123@ds225375.mlab.com:25375/api-full-nodejs', {
+   useNewUrlParser: true
+});
+
+// CARREGANDO OS MODELS
+requireDir('./models');
 
 // ROTAS
-const indexRoute = require('./routes/Index'); 
+const indexRoute = require('./routes/Index');
 const productRoute = require('./routes/ProductRoute');
 
 app.use(express.json());
