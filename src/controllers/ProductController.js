4262f2f4245ca2d290c3068b2a57bcf3449814ae
Thiagosf-dev <thiagosf.dev@gmail.com>
2019-03-25 20:03:38 -0400

    Finalziado CRUD básico no Controller

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -5,22 +5,44 @@ const Product = mongoose.model('Product');
 
 module.exports = {
    async getAll(req, res, next) {
-      return res.send("todos");
+      return res.json(await Product.find({
+         active: true
+      }, 'title price slug'));
+   },
+
+   async getBySlug(req, res, next) {
+      return res.json(await Product.findOne({
+         active: true,
+         slug: req.params.slug
+      }, 'title description price slug'))
+   },
+
+   async getById(req, res, next) {
+      return res.json(await Product.findById(req.params.id));
+   },
+
+   async getByTag(req, res, next) {
+      return res.json(await Product.find({
+         tags: req.params.tag,
+         active: true
+      }, 'title description price slug tags'));
    },
 
    async insert(req, res, next) {
       try {
          return res.json(await Product.create(req.body));
       } catch (error) {
-         return res.send(error);  
+         return res.send(error);
       }
    },
 
    async update(req, res, next) {
-      return res.send("update");
+      return res.json(await Product.findByIdAndUpdate(req.params.id, req.body, {
+         new: true
+      }));
    },
 
    async remove(req, res, next) {
-      return res.send("remove");
+      return res.json(await Product.findByIdAndRemove(req.body.id));
    },
 };
--- src/routes/ProductRoute.js
+++ src/routes/ProductRoute.js
@@ -7,6 +7,12 @@ const controller = require('../controllers/ProductController');
 router.get('/', controller.getAll)
    .post('/', controller.insert)
    .put('/:id', controller.update)
-   .delete('/:id', controller.remove);
+   .delete('/', controller.remove);
+
+router.get('/:slug', controller.getBySlug);
+
+router.get('/get/:id', controller.getById);
+
+router.get('/tags/:tag', controller.getByTag);
 
 module.exports = router;
